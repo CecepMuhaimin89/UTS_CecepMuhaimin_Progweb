ac0f11ecc0e46fc1080c4adf3b3e59bff3eb5f6c
CECEP MUHAIMIN <laptopstore_indonesia@192.168.1.7>
2022-05-24 00:30:25 +0700

    first commit

---
+++ resources/views/datamahasiswa.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UTS Cecep Muhaimin</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
+
+        th,
+        td {
+            padding: 10px;
+        }
+
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+    </style>
+</head>
+
+<body>
+    <h1>Data Mahasiswa Teknik Informatika</h1>
+    <table>
+        <tr>
+            <th>NIM</th>
+            <th>Nama Lengkap</th>
+            <th>No. Telp</th>
+            <th>Alamat</th>
+        </tr>
+        <tr>
+            <td>20200801341</td>
+            <td>Cecep Muhaimin</td>
+            <td>082121525369</td>
+            <td>Bandung</td>
+        </tr>
+        <tr>
+            <td>20200801432</td>
+            <td>Teja Paku Alam</td>
+            <td>082240608618</td>
+            <td>Palembang</td>
+        </tr>
+        <tr>
+            <td>20200801554</td>
+            <td>Egy Maulana Vikri</td>
+            <td>087288678909</td>
+            <td>Medan</td>
+        </tr>
+        <tr>
+            <td>2020080682</td>
+            <td>irfan Jaya</td>
+            <td>081345678932</td>
+            <td>Jakarta</td>
+        </tr>
+    </table>
+</body>
+
+</html>
